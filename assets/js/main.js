a463c2bd2e89ff0c7f2c58e5325944ffbd1ca758
kaushal <kaushal.chitlangia97@gmail.com>
2018-08-31 00:46:50 +0530

    first

---
+++ assets/js/main.js
@@ -0,0 +1,155 @@
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
+});
+
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid'); 
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid'); 
+	}
+}
+
+/*
+ * Registration Form
+*/
+
+$('#registration-form').submit(function(e){
+    e.preventDefault();
+    
+    var postForm = { //Fetch form data
+            'fname'     : $('#registration-form #fname').val(),
+            'lname'     : $('#registration-form #lname').val(),
+            'email'     : $('#registration-form #email').val(),
+            'cell'      : $('#registration-form #cell').val(),
+            'address'   : $('#registration-form #address').val(),
+            'zip'       : $('#registration-form #zip').val(),
+            'city'      : $('#registration-form #city').val(),
+            'program'   : $('#registration-form #program').val()
+    };
+
+    $.ajax({
+            type      : 'POST',
+            url       : './assets/php/contact.php',
+            data      : postForm,
+            dataType  : 'json',
+            success   : function(data) {
+                            if (data.success) {
+                                $('#registration-msg .alert').html("Registration Successful");
+                                $('#registration-msg .alert').removeClass("alert-danger");
+                                $('#registration-msg .alert').addClass("alert-success");
+                                $('#registration-msg').show();
+                            }
+                            else
+                            {
+                                $('#registration-msg .alert').html("Registration Failed");
+                                $('#registration-msg .alert').removeClass("alert-success");
+                                $('#registration-msg .alert').addClass("alert-danger");
+                                $('#registration-msg').show();
+                            }
+                        }
+        });
+});
+
+/*
+ * SmoothScroll
+*/
+
+smoothScroll.init();
+
+
+$(document).ready(function(){
+    
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
+
+})
+
+
+$(document).ready(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
